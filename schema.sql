c831405a9203af0ae417594c6328b86e2fd9406a
AshenKavinda <kavindahemarathna321@gmail.com>
2025-08-31 12:37:49 +0530

    update schema

--- schema.sql
+++ schema.sql
@@ -8,7 +8,6 @@ CREATE TABLE user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    otp VARCHAR(6),
     role ENUM('manager','teacher','officer','guest') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
